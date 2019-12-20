9cb3e859211841f0d1a4e36a11e400f91287b7c1
jab0707 <jab0707@westminstercollege.edu>
2019-12-20 08:27:06 -0700

    working

--- Processing/CompareInvResults.m
+++ Processing/CompareInvResults.m
@@ -14,8 +14,30 @@ dataDir = '/home/sci/jbergquist/projects/WorkingData/InterpolationGroup/Tikhonov
 outDir = '/home/sci/jbergquist/projects/WorkingData/InterpolationGroup/TikhonovRegResults/Vis/';
 
 
-grndTruth = load([dataDir,'EGMTrutBeats.mat']);
+grndTruth = load([dataDir,'EGMTrueBeats.mat']);
 
 
 
 fNameBase = 'X_Tikh_Case2_Pacing_Int_5_';
+
+for pt2 = 1:5
+    for interp = 1:7
+        
+        try
+           load([dataDir,fNameBase,sprintf('%d_%d.mat',pt2,interp)]);
+           
+           for b = 1:31
+               currBeat = X_Tikh{b};
+               
+               [ RMSE, RMSE_space, RMSE_time, PCC, mean_PCC, CorrVal, mean_CorrVal,Diff,maxDiff,minDiff ] = CalculateSignalStatistics( currBeat.VeSingLam(1:108,:), grndTruth.EGMtrue{b} );
+               save([outDir,fNameBase,sprintf('%d_%d_RMSE_Space_b%d.mat',pt2,interp,b)],'RMSE_space');
+               save([outDir,fNameBase,sprintf('%d_%d_RMSE_CorrVal_b%d.mat',pt2,interp,b)],'RMSE_space');
+           end
+            
+        catch
+            fprintf(['Failed at ',fNameBase,sprintf('%d_%d\n',pt2,interp)]);
+        end
+        
+        
+    end
+end
